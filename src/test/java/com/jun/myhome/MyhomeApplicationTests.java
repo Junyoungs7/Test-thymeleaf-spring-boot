422baa77b36f4107f4aac753b4ca50d7933032d2
junyoung <junyoungs7@naver.com>
2022-07-15 18:38:59 +0900

    init

---
+++ src/test/java/com/jun/myhome/MyhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jun.myhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
