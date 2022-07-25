8aeceadfaccccb09ee1b07326009629c91c7b77c
junyoung <junyoungs7@naver.com>
2022-07-25 23:18:19 +0900

    init

--- src/main/java/com/jun/myhome/repository/BoardRepository.java
+++ src/main/java/com/jun/myhome/repository/BoardRepository.java
@@ -3,5 +3,9 @@ package com.jun.myhome.repository;
 import com.jun.myhome.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title, String content);
 }
